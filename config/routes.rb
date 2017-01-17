5856bc75a986b7a031129d688a132e1d965daa04
Matthew Licausi <matthew@licausidev.net>
2017-01-17 11:41:41 -0600

    load up player section and move business into app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :players
+  
+  get 'player', to: 'players#index'
+  
+  resources :businesses
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'business', to: 'businesses#index'
 end
